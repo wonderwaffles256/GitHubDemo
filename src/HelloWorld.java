59cb56e2da5928463f9074ac3602314869340bc4
SCHULZEOK21 <oschulze03@gmail.com>
2022-11-07 13:49:51 -0500

    commitment is hard

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        System.out.println("greetings");
         System.out.println("I broke it");
+        System.out.println("i fixed it");
     }
 }
